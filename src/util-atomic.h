fa79870a2da00c834ee219ad1ae17972ad6ff2a7
Tom DeCanio <decanio.tom@gmail.com>
2011-12-12 14:33:41 -0800

    remove unused compiler warning

--- src/util-atomic.h
+++ src/util-atomic.h
@@ -45,8 +45,6 @@
 #if !defined(__GCC_HAVE_SYNC_COMPARE_AND_SWAP_8) || !defined(__GCC_HAVE_SYNC_COMPARE_AND_SWAP_4) || \
     !defined(__GCC_HAVE_SYNC_COMPARE_AND_SWAP_2) || !defined(__GCC_HAVE_SYNC_COMPARE_AND_SWAP_1)
 
-//#warning seem to have atomics
-
 #ifndef __tilegx__
 /**
  *  \brief wrapper to declare an atomic variable including a (spin) lock
