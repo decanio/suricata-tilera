b54a6664a2b36ccff750d4504e47a09dc759aae4
Tom DeCanio <decanio.tom@gmail.com>
2012-06-12 18:51:08 -0700

    added tmcqueue packet queueing

---
+++ src/tmqh-demux2.c
@@ -0,0 +1,217 @@
+/* Copyright (C) 2007-2010 Open Information Security Foundation
+ *
+ * You can copy, redistribute or modify this Program under the terms of
+ * the GNU General Public License version 2 as published by the Free
+ * Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+/**
+ * \file
+ *
+ * \author Victor Julien <victor@inliniac.net>
+ *
+ * 2-way demultiplexing queue handler
+ */
+
+#include "suricata.h"
+#include "packet-queue.h"
+#include "decode.h"
+#include "threads.h"
+#include "threadvars.h"
+
+#include "tm-queuehandlers.h"
+
+#ifdef __tile__
+#include <arch/cycle.h>
+#include <tmc/mem.h>
+#endif
+
+Packet *TmqhInputDemux2(ThreadVars *t);
+void TmqhOutputDemux2(ThreadVars *t, Packet *p);
+void TmqhInputDemux2ShutdownHandler(ThreadVars *);
+
+void TmqhDemux2Register (void) {
+    tmqh_table[TMQH_DEMUX2].name = "demux2";
+    tmqh_table[TMQH_DEMUX2].InHandler = TmqhInputDemux2;
+    tmqh_table[TMQH_DEMUX2].InShutdownHandler = TmqhInputDemux2ShutdownHandler;
+    tmqh_table[TMQH_DEMUX2].OutHandler = TmqhOutputDemux2;
+}
+
+#ifdef __tile__
+static void
+cycle_pause(unsigned int delay)
+{
+  const unsigned int start = get_cycle_count_low();
+  while (get_cycle_count_low() - start < delay)
+    ;
+}
+#endif
+
+Packet *TmqhInputDemux2(ThreadVars *t)
+{
+    PacketQueue *q = &trans_q[t->inq->id];
+
+    SCPerfSyncCountersIfSignalled(t, 0);
+
+    SCMutexLock(&q->mutex_q);
+
+    if (unlikely(q->len == 0)) {
+        /* if we have no packets in queue, wait... */
+#ifdef __tile__
+        do {
+            SCMutexUnlock(&q->mutex_q);
+            while ((q->len == 0)/* && (q->cond_q == 0)*/) {
+	            cycle_pause(300);
+            }
+            SCMutexLock(&q->mutex_q);
+        } while ((q->len == 0)/* && (q->cond_q == 0)*/);
+#else
+        SCCondWait(&q->cond_q, &q->mutex_q);
+#endif
+    }
+
+    if (likely(q->len > 0)) {
+        Packet *p = PacketDequeue(q);
+#ifdef __tile__
+        if (q->len == 0) q->cond_q = 0;
+#endif
+        SCMutexUnlock(&q->mutex_q);
+        return p;
+    } else {
+        /* return NULL if we have no pkt. Should only happen on signals. */
+#ifdef __tile__
+        q->cond_q = 0;
+#endif
+        SCMutexUnlock(&q->mutex_q);
+        return NULL;
+    }
+}
+
+void TmqhInputDemux2ShutdownHandler(ThreadVars *tv) {
+    int i;
+
+    if (tv == NULL || tv->inq == NULL) {
+        return;
+    }
+
+    for (i = 0; i < (tv->inq->reader_cnt + tv->inq->writer_cnt); i++)
+#ifdef __tile__
+        trans_q[tv->inq->id].cond_q = 1;
+#else
+        SCCondSignal(&trans_q[tv->inq->id].cond_q);
+#endif
+}
+
+void TmqhOutputDemux2(ThreadVars *t, Packet *p)
+{
+    int n = t->outq->id + (p->pool & 1);
+    SCLogDebug("Packet %p, p->root %p, alloced %s", p, p->root, p->flags & PKT_ALLOC ? "true":"false");
+    //SCLogInfo("Packet %p, id %d q %d p->pool %d, p->root %p, alloced %s", p, n, t->outq->id, p->pool, p->root, p->flags & PKT_ALLOC ? "true":"false");
+
+    PacketQueue *q = &trans_q[n];
+
+    SCMutexLock(&q->mutex_q);
+    PacketEnqueue(q, p);
+#ifdef __tile__
+    q->cond_q = 1;
+    //tmc_mem_fence();
+    SCMutexUnlock(&q->mutex_q);
+#else
+    SCCondSignal(&q->cond_q);
+    SCMutexUnlock(&q->mutex_q);
+#endif
+}
+
+/*******************************Generic-Q-Handlers*****************************/
+
+/**
+ * \brief Public version of TmqhInputSimple from the tmqh-simple queue
+ *        handler, except that it is a generic version that is directly
+ *        tied to a "SCDQDataQueue" instance(sent as an arg).
+ *
+ *        Retrieves a data_instance from the queue.  If the queue is empty, it
+ *        waits on the queue, till a data_instance is enqueued into the queue
+ *        by some other module.
+ *
+ *        All references to "data_instance" means a reference to a data structure
+ *        instance that implements the template "struct SCDQGenericQData_".
+ *
+ * \param q The SCDQDataQueue instance to wait on.
+ *
+ * \retval p The returned packet from the queue.
+ * \retval data The returned data_instance from the queue.
+ */
+SCDQGenericQData *TmqhInputDemux2OnQ(SCDQDataQueue *q)
+{
+    SCMutexLock(&q->mutex_q);
+    if (q->len == 0) {
+        /* if we have no packets in queue, wait... */
+#ifdef __tile__
+        for (;;) {
+            if ((q->len > 0) || q->cond_q)
+                break;
+            SCMutexUnlock(&q->mutex_q);
+            cycle_pause(4300);
+            SCMutexLock(&q->mutex_q);
+        }
+#else
+        SCCondWait(&q->cond_q, &q->mutex_q);
+#endif
+    }
+
+    if (q->len > 0) {
+        SCDQGenericQData *data = SCDQDataDequeue(q);
+#ifdef __tile__
+        if (q->len == 0) q->cond_q = 0;
+#endif
+        SCMutexUnlock(&q->mutex_q);
+        return data;
+    } else {
+        /* return NULL if we have no data in the queue. Should only happen
+         * on signals. */
+#ifdef __tile__
+        q->cond_q = 0;
+#endif
+        SCMutexUnlock(&q->mutex_q);
+        return NULL;
+    }
+}
+
+/**
+ * \brief Public version of TmqhOutputSimple from the tmqh-simple queue
+ *        handler, except that it is a generic version that is directly
+ *        tied to a SCDQDataQueue instance(sent as an arg).
+ *
+ *        Pumps out a data_instance into the queue.  If the queue is empty, it
+ *        waits on the queue, till a data_instance is enqueued into the queue.
+ *
+ *        All references to "data_instance" means a reference to a data structure
+ *        instance that implements the template "struct SCDQGenericQData_".
+ *
+ * \param q    The SCDQDataQueue instance to pump the data into.
+ * \param data The data instance to be enqueued.
+ */
+void TmqhOutputDemux2OnQ(SCDQDataQueue *q, SCDQGenericQData *data)
+{
+    SCMutexLock(&q->mutex_q);
+    SCDQDataEnqueue(q, data);
+#ifdef __tile__
+    q->cond_q = 0;
+    SCMutexUnlock(&q->mutex_q);
+#else
+    SCCondSignal(&q->cond_q);
+    SCMutexUnlock(&q->mutex_q);
+#endif
+
+    return;
+}
---
+++ src/tmqh-tmcqueue.c
@@ -0,0 +1,185 @@
+/* Copyright (C) 2007-2010 Open Information Security Foundation
+ *
+ * You can copy, redistribute or modify this Program under the terms of
+ * the GNU General Public License version 2 as published by the Free
+ * Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+/**
+ * \file
+ *
+ * \author Tom DeCanio <decanio.tom@gmail.com>
+ *
+ * TMC queue handler
+ */
+
+#include "suricata.h"
+#include "packet-queue.h"
+#include "decode.h"
+#include "threads.h"
+#include "threadvars.h"
+
+#include "tm-queuehandlers.h"
+
+#ifdef __tile__
+
+/* modeled after Tilera TMC queue */
+#define LOG2_PACKETQ_ENTRIES 11 /* 2048 entries */
+typedef struct TmcPacketQueue_ {
+    tmc_spin_queued_mutex_t enqueue_mutex;
+    unsigned int enqueue_count;
+
+    tmc_spin_queued_mutex_t dequeue_mutex __attribute__((aligned(64)));
+    unsigned int dequeue_count;
+
+    Packet *array[1<<LOG2_PACKETQ_ENTRIES] __attribute__((aligned(64)));
+} TmcPacketQueue;
+
+
+static TmcPacketQueue *tmcqueues[256];
+
+Packet *TmqhInputTmcQueueMrSw(ThreadVars *t);
+void TmqhOutputTmcQueueMrSw(ThreadVars *t, Packet *p);
+Packet *TmqhInputTmcQueueSrSw(ThreadVars *t);
+void TmqhOutputTmcQueueSrSw(ThreadVars *t, Packet *p);
+Packet *TmqhInputTmcQueueSrMw(ThreadVars *t);
+void TmqhOutputTmcQueueSrMw(ThreadVars *t, Packet *p);
+void TmqhInputTmcQueueShutdownHandler(ThreadVars *);
+
+/**
+ * \brief TmqhTmcQueueRegister
+ * \initonly
+ */
+void TmqhTmcQueueRegister (void) {
+    tmqh_table[TMQH_RINGBUFFER_MRSW].name = "tmc_mrsw";
+    tmqh_table[TMQH_RINGBUFFER_MRSW].InHandler = TmqhInputTmcQueueMrSw;
+    tmqh_table[TMQH_RINGBUFFER_MRSW].InShutdownHandler = TmqhInputTmcQueueShutdownHandler;
+    tmqh_table[TMQH_RINGBUFFER_MRSW].OutHandler = TmqhOutputTmcQueueMrSw;
+
+    tmqh_table[TMQH_RINGBUFFER_SRSW].name = "tmc_srsw";
+    tmqh_table[TMQH_RINGBUFFER_SRSW].InHandler = TmqhInputTmcQueueSrSw;
+    tmqh_table[TMQH_RINGBUFFER_SRSW].InShutdownHandler = TmqhInputTmcQueueShutdownHandler;
+    tmqh_table[TMQH_RINGBUFFER_SRSW].OutHandler = TmqhOutputTmcQueueSrSw;
+
+    tmqh_table[TMQH_RINGBUFFER_SRMW].name = "tmc_srmw";
+    tmqh_table[TMQH_RINGBUFFER_SRMW].InHandler = TmqhInputTmcQueueSrMw;
+    tmqh_table[TMQH_RINGBUFFER_SRMW].InShutdownHandler = TmqhInputTmcQueueShutdownHandler;
+    tmqh_table[TMQH_RINGBUFFER_SRMW].OutHandler = TmqhOutputTmcQueueSrMw;
+
+    memset(tmcqueues, 0, sizeof(tmcqueues));
+
+    int i = 0;
+    for (i = 0; i < 256; i++) {
+        tmcqueues[i] = RingBuffer8Init();
+        if (tmcqueues[i] == NULL) {
+            SCLogError(SC_ERR_FATAL, "Error allocating memory to register Ringbuffers. Exiting...");
+            exit(EXIT_FAILURE);
+        }
+    }
+}
+
+void TmqhInputRingBufferShutdownHandler(ThreadVars *tv) {
+    if (tv == NULL || tv->inq == NULL) {
+        return;
+    }
+
+    TmcPacketQueue *rb = tmcqueues[tv->inq->id];
+    if (rb == NULL) {
+        return;
+    }
+
+    RingBuffer8Shutdown(rb);
+}
+
+static void Enqueue (TmcPacketQueue *q, Packet *p) {
+    //SCLogInfo("PacketEnqueue %d %d %p", q->enqueue_count, q->dequeue_count, p);
+    while ((q->enqueue_count - atomic_access_once(q->dequeue_count)) >=
+        (1 << LOG2_PACKETQ_ENTRIES)) {
+        ;
+    }
+    unsigned int enqueue_count = q->enqueue_count;
+    unsigned int i = enqueue_count & ((1 << LOG2_PACKETQ_ENTRIES) - 1);
+    q->array[i] = p;
+    __insn_mf();
+    q->enqueue_count = enqueue_count + 1;
+    //SCLogInfo("PacketEnqueue done %d %d", q->enqueue_count, q->dequeue_count);
+}
+
+static Packet *Dequeue (TmcPacketQueue *q) {
+    Packet *p = NULL;
+    //SCLogInfo("PacketDequeue %d %d", q->enqueue_count, q->dequeue_count);
+    if (atomic_access_once(q->enqueue_count) == q->dequeue_count) {
+        return p;
+    } else {
+      unsigned int dequeue_count = q->dequeue_count;
+      unsigned int i = dequeue_count & ((1 << LOG2_PACKETQ_ENTRIES) - 1);
+      p = q->array[i];
+      __insn_mf(); /* Make sure loads are back. */
+      q->dequeue_count = dequeue_count + 1;
+    }
+    //SCLogInfo("PacketDequeue %d %d %p", q->enqueue_count, q->dequeue_count, p);
+    return p;
+}
+
+
+Packet *TmqhInputTmcQueueMrSw(ThreadVars *t)
+{
+    TmcPacketQueue *rb = tmcqueues[t->inq->id];
+
+    Packet *p = (Packet *)Dequeue(rb);
+
+    SCPerfSyncCountersIfSignalled(t, 0);
+
+    return p;
+}
+
+void TmqhOutputTmcQueueMrSw(ThreadVars *t, Packet *p)
+{
+    TmcPacketQueue *rb = tmcqueues[t->outq->id];
+    Enqueue(rb, (void *)p);
+}
+
+Packet *TmqhInputTmcQueueSrSw(ThreadVars *t)
+{
+    TmcPacketQueue *rb = tmcqueues[t->inq->id];
+
+    Packet *p = (Packet *)Dequeue(rb);
+
+    SCPerfSyncCountersIfSignalled(t, 0);
+
+    return p;
+}
+
+void TmqhOutputTmcQueueSrSw(ThreadVars *t, Packet *p)
+{
+    TmcPacketQueue *rb = tmcqueues[t->outq->id];
+    Enqueue(rb, (void *)p);
+}
+
+Packet *TmqhInputTmcQueueSrMw(ThreadVars *t)
+{
+    TmcPacketQueue *rb = tmcqueues[t->inq->id];
+
+    Packet *p = (Packet *)Dequeue(rb);
+
+    SCPerfSyncCountersIfSignalled(t, 0);
+
+    return p;
+}
+
+void TmqhOutputTmcQueueSrMw(ThreadVars *t, Packet *p)
+{
+    TmcPacketQueue *rb = tmcqueues[t->outq->id];
+    Enqueue(rb, (void *)p);
+}
+#endif
